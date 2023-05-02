42505f8b38259a5c562b1d909f2ddfd9373fc8c4
Armand Krynauw <armand.krynauw@gmail.com>
2023-05-02 16:26:35 +0200

    Upgrade all packages except rollup

--- src/frontend/App.svelte
+++ src/frontend/App.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <main>
-  <div class="flex flex-col items-center justify-center min-h-screen bg-stone-700">
+  <div class="flex flex-col items-center justify-center">
     <h1 class="text-3xl md:text-5xl text-orange-500 font-bold mb-5">Hello {name}!</h1>
   </div>
 </main>
