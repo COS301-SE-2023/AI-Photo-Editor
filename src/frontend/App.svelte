a279fc888c229097095977ef726634461db8a420
Rec1dite <rec1dite@gmail.com>
2023-05-02 17:18:29 +0200

    Add svelte-splitpanes

--- src/frontend/App.svelte
+++ src/frontend/App.svelte
@@ -2,14 +2,39 @@
   @tailwind base;
   @tailwind components;
   @tailwind utilities;
+
+  :global(.splitpanes__pane) {
+  box-shadow: 0 0 3px rgba(0, 0, 0, .2) inset;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  position: relative;
+}
+
 </style>
 
 <script lang="ts">
-  let name = "Jake";
+  import { Pane, Splitpanes } from 'svelte-splitpanes';
+
+  let v = 10
+
 </script>
 
-<main>
-  <div class="flex flex-col items-center justify-center">
-    <h1 class="text-3xl md:text-5xl text-orange-500 font-bold mb-5">Hello {name}!</h1>
-  </div>
-</main>
+<Splitpanes class="default-theme" style="height: 400px">
+	<Pane minSize={v}>I have a min width of 20%</Pane>
+
+	<Pane>
+
+		<Splitpanes class="default-theme" horizontal="{true}">
+
+			<Pane minSize={15}>I have a min height of 15%</Pane>
+			<Pane>Try double clicking the splitters</Pane>
+			<Pane snapSize={10}>With a bottom snap of 10</Pane>
+
+		</Splitpanes>
+
+	</Pane>
+
+	<Pane>Right Panel</Pane>
+
+</Splitpanes>
