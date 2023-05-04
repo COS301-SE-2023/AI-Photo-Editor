6153291d13963630f52f7dbec416efff4c663bd6
Armand Krynauw <armand.krynauw@gmail.com>
2023-05-04 14:59:20 +0200

    Add custom dark theme for the node graph

--- src/frontend/components/Graph.svelte
+++ src/frontend/components/Graph.svelte
@@ -2,8 +2,26 @@
 	import { Svelvet, ThemeToggle, Node, Background } from "svelvet";
 </script>
   
+
 <Svelvet id="my-canvas" zoom={0.5} minimap theme="custom-theme">
 	<Node bgColor="#ec4899" height={200} position={{ x: 100, y: 100 }} />
-	<ThemeToggle main="dark" alt="light" slot="toggle"/>
-	<Background dotColor="grey" bgColor="#27272a" gridWidth={40} dotSize={2} slot="background" />
+	<ThemeToggle main="custom-dark" alt="light" slot="toggle" mainIcon="light_mode"/>
 </Svelvet> 
+
+
+<style>
+	:root[svelvet-theme='custom-dark'] {
+		--background-color: #27272a;
+		--dot-color: hsl(225, 10%, 50%);
+
+		--minimap-background-color: #52525b;
+
+		--minimap-node-color: hsl(225, 30%, 20%);
+
+		--controls-background-color: hsl(225, 20%, 27%);
+		--controls-text-color: hsl(0, 0%, 100%);
+
+		--theme-toggle-text-color: hsl(0, 0%, 100%);
+		--theme-toggle-color: hsl(225, 20%, 27%);
+	}
+</style>
