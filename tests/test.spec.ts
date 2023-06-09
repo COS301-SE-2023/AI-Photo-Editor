7c30e58eac8e87578a319bd979e9112b0b7c34c3
CenturionLC <johnshepard357@gmail.com>
2023-06-09 12:50:16 +0200

    Improved Testing structure

--- tests/test.spec.ts
+++
@@ -1,9 +0,0 @@
-import { firstTest, secondTest } from "./test1";
-
-test('firstTest function', () => {
-    expect(firstTest()).toBe("Test Working");
-});
-
-test('secondTest function', () => {
-    expect(secondTest()).toBe("Test Working");
-});
