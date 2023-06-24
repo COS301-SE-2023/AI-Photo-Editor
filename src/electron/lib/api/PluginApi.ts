20faf901e7f37029310f19a261c232a9d46ef416
Klairgo <olwagekarel@gmail.com>
2023-06-24 11:22:01 +0200

    Update plugin integration with frontend

--- src/electron/lib/api/PluginApi.ts
+++ src/electron/lib/api/PluginApi.ts
@@ -1,28 +1,25 @@
 import type { ElectronMainApi } from "electron-affinity/main";
 import { CommandInstance, CommandRegistry } from "../commands/CommandRegistry";
 import type { Blix } from "../Blix";
-import { PluginManager } from "../plugins/PluginManager";
 import logger from "../../utils/logger";
 
 export class PluginApi implements ElectronMainApi<PluginApi> {
   private _counter = 0;
-  private _registry: { [key: string]: CommandInstance };
   private readonly _blix: Blix;
 
   constructor(blix: Blix) {
     this._blix = blix;
-    this._registry = this._blix.commandRegistry.getRegistry();
   }
 
   async addCommand(instance: CommandInstance) {
     this._blix.commandRegistry.addInstance(instance);
   }
 
-  async runCommand() {
-    // this._blix.commandRegistry.getRegistry();
+  async runCommand(command: string) {
+    this._blix.commandRegistry.runCommand(command);
   }
 
   async getCommands() {
-    return this._blix.commandRegistry.getCommands();
+    return this._blix.commandRegistry.getCommandNames();
   }
 }
