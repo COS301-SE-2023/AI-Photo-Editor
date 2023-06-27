4913e785b4c58a8763ad1af9e26960f7d2cc9e26
Klairgo <olwagekarel@gmail.com>
2023-06-27 09:14:13 +0200

    Add integration tests for Plugin

--- tests/integration-tests/plugins/plugin.spec.ts
+++ tests/integration-tests/plugins/plugin.spec.ts
@@ -1,12 +1,47 @@
 import expect from "expect";
 import { NodeInstance,InputAnchorInstance,OutputAnchorInstance, NodeUIParent } from "../../../src/electron/lib/core-graph/ToolboxRegistry";
 import {NodeBuilder,NodeUIBuilder} from "../../../src/electron/lib/plugins/builders/NodeBuilder"
+// import { ProjectManager } from "../../../src/electron/lib/projects/ProjectManager";
 import { Plugin } from "../../../src/electron/lib/plugins/Plugin";
 import { Blix } from "../../../src/electron/lib/Blix";
 import { MainWindow } from "../../../src/electron/lib/api/WindowApi";
 import {BrowserWindow} from "electron";
 import { join } from "path";
+import { writeFileSync } from "fs";
 
+
+const mainWindow: MainWindow = {
+  apis: {
+    commandRegistryApi: jest.fn(),
+    clientGraphApi: jest.fn(),
+    clientProjectApi: jest.fn()
+    
+  }
+} as any;
+
+jest.mock("../../../src/electron/lib/projects/ProjectManager");
+
+jest.mock("electron", () => ({
+  app: {
+    getPath: jest.fn((path) => {
+      return "test/electron";
+    }),
+    getName: jest.fn(() => {
+      return "TestElectron";
+    }),
+    getVersion: jest.fn(() => {
+      return "v1.1.1";
+    })
+  },
+}));
+
+jest.mock("fs", () => ({
+  readFileSync: jest.fn().mockReturnValue("mocked_base64_string"),
+  readFile: jest.fn((filePath, callback) => callback(null, "mocked_file_data")),
+  existsSync: jest.fn(),
+  mkdirSync: jest.fn(),
+  writeFileSync: jest.fn(),
+}));
 
 describe("Test builder propagations", () => {
 
@@ -51,8 +86,6 @@ describe("Test builder propagations", () => {
 describe("Test plugin integrations", () => {
     let plugin : Plugin;
     let blix : Blix;
-    let mainWindow : MainWindow;
-
 
     const pack : any = {
         name: 'hello-plugin',
@@ -68,43 +101,49 @@ describe("Test plugin integrations", () => {
         comments: [ 'This property will be completely ignored' ]
       };
 
-      mainWindow = new BrowserWindow({
-        width: 1300,
-        height: 1000,
-        webPreferences: {
-          devTools: !true,
-          contextIsolation: true,
-          nodeIntegration: false,
-          sandbox: true,
-          preload: join(__dirname, "preload.js"),
-        },
-        // Set icon for Windows and Linux
-        icon: "public/images/blix_64x64.png",
-        titleBarStyle: "hidden",
-        trafficLightPosition: { x: 10, y: 10 },
-      }) as MainWindow;
 
-      
-
-    const plugDir : string = "/home/centurion/Desktop/Centurion/Coding/301/Capstone/AI-Photo-Editor/blix-plugins/base-plugin";
-    const main : string = "/home/centurion/Desktop/Centurion/Coding/301/Capstone/AI-Photo-Editor/blix-plugins/base-plugin/src/main.js";
+    const plugDir : string = "../../../../blix-plugins/hello-plugin";
+    const main : string = "../../../../blix-plugins/hello-plugin/src/main.js";
 
 
     beforeEach(() => {
       jest.clearAllMocks();
       plugin = new Plugin(pack,plugDir,main);
       blix = new Blix(mainWindow);
+      plugin.requireSelf(blix);
     });
 
     test("Plugin should send nodes to toolbox registry", () => {
-
         plugin.requireSelf(blix);
 
-        const tools =  Object.values(blix.toolbox.getRegistry);
-        expect(blix.toolbox.getRegistry.length).toEqual(2);
+        const tools =  Object.values(blix.toolbox.getRegistry());
+        expect(tools.length).toEqual(2);
       });
-  
-      test("addDropdown should affect nodeUi's children", () => {
 
-      });
+    test("Plugin should send commands to command registry", () => {
+        plugin.requireSelf(blix);
+
+        const commands =  Object.values(blix.commandRegistry.getRegistry());
+        expect(commands.length).toEqual(3);
+    })
+
+    test("Plugin should be able to run", () => {
+      console.log = jest.fn();
+      plugin.requireSelf(blix);
+
+      const commands =  blix.commandRegistry.getRegistry()["hello-plugin.import"];
+      commands.run();
+      expect(console.log).toHaveBeenCalled();
+
+    })
+
+    test("Command registry should have correct contents", () => {
+      plugin.requireSelf(blix);
+
+      const commands =  blix.commandRegistry.getRegistry();
+      expect(commands.hasOwnProperty("hello-plugin.import")).toBeTruthy();
+      expect(commands.hasOwnProperty("hello-plugin.export")).toBeTruthy();
+      expect(commands.hasOwnProperty("hello-plugin.addBrightnessNode")).toBeTruthy();
+
+    })
 });
