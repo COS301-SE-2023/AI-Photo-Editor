e5c992053eb2646fea4960fa372c428f23d69962
CenturionLC <johnshepard357@gmail.com>
2023-07-10 19:46:54 +0200

    Add jest docs and dummy graph

--- src/electron/lib/ai/AiManager.ts
+++ src/electron/lib/ai/AiManager.ts
@@ -5,15 +5,48 @@ import type { MainWindow } from "../api/apis/WindowApi";
 import prompt from "electron-prompt";
 import { error } from "console";
 import { OpenAI } from "langchain/llms/openai";
-import { OpenAIEmbeddings } from "langchain/embeddings";
-import { Interface } from "readline";
-import { Blix } from "../Blix";
 import { NodeInstance, ToolboxRegistry } from "../registries/ToolboxRegistry";
+
+// Refer to .env for api keys
+
+//  TODO : Provide the graph context that will be given to ai as context
 
 interface Nodes {
   name: string;
   description: string;
 }
+
+const graph = {
+  nodes: [
+    {
+      signature: "input-plugin.number1",
+      inputs: [],
+      outputs: [5],
+    },
+    {
+      signature: "input-plugin.number2",
+      inputs: [],
+      outputs: [7],
+    },
+    {
+      signature: "math-plugin.add",
+      inputs: [5, 7],
+      outputs: [12],
+    },
+    {
+      signature: "output-plugin.print",
+      inputs: [12],
+      outputs: [],
+    },
+  ],
+};
+
+/**
+ *
+ * Manages ai by storing context and handling prompt input
+ * @param mainWindow Main window of blix application
+ *
+ * */
 export class AiManager {
   private _path: string;
   private _mainWindow;
@@ -27,6 +60,13 @@ export class AiManager {
     }
   }
 
+  /**
+   *
+   * Initializes context of ai with all the nodes in the toolbox
+   * @param toolbox This is the blix toolbox registry that contains all the nodes
+   *
+   * */
+
   instantiate(toolbox: ToolboxRegistry): void {
     for (const index in toolbox.getRegistry()) {
       if (!toolbox.hasOwnProperty(index)) {
@@ -37,17 +77,28 @@ export class AiManager {
     // console.log(this._context);
   }
 
+  /**
+   *
+   * Creates prompt window for the user to input a prompt for the AI to respond to
+   */
+
   async sendPrompt() {
     //  Creates a prompt window for the user to input a prompt for the AI to respond to
     const prompt = await this.createInputWindow();
     if (prompt === "undefined") return;
 
     // Manipulate prompt
-    const model = new OpenAI({ openAIApiKey: process.env.OPENAI_API_KEY, temperature: 0.3 });
+    const model = new OpenAI({ openAIApiKey: process.env.OPENAI_API_KEY, temperature: 0.1 });
     model.modelName = "text-ada-001";
     const res = await model.call(prompt);
     // console.log(res);
   }
+
+  /**
+   *
+   *
+   * @returns A promise that resolves with the prompt value
+   */
 
   async createInputWindow(): Promise<string> {
     return new Promise((resolve, reject) => {
--- src/electron/lib/plugins/builders/NodeBuilder.ts
+++ src/electron/lib/plugins/builders/NodeBuilder.ts
@@ -2,6 +2,12 @@ import { type PluginContextBuilder } from "./PluginContextBuilder";
 import { NodeInstance } from "../../registries/ToolboxRegistry";
 import { NodeUIComponent, NodeUILeaf, NodeUIParent } from "../../../../shared/ui/NodeUITypes";
 
+/**
+ *
+ * Builds a node through restricted interface
+ * @param node NodeInstance
+ *
+ * */
 export class NodeBuilder implements PluginContextBuilder {
   constructor(node: NodeInstance) {
     this.node = node;
@@ -11,6 +17,12 @@ export class NodeBuilder implements PluginContextBuilder {
   private node: NodeInstance;
   private ui: NodeUIParent | null;
 
+  /**
+   * Ensures that nodeui and is instantiated
+   * @throws Error if node is not instantiated
+   *
+   */
+  // This might be redundant, should look into it
   public validate(): void {
     if (this.node.getSignature === "" || this.node.getSignature === null) {
       throw new Error("Node is not instantiated");
@@ -22,9 +34,16 @@ export class NodeBuilder implements PluginContextBuilder {
     return;
   }
 
+  // Why do users need this?
   get build(): any {
     return null;
   }
+
+  /**
+   *
+   * @param title Title of the node
+   *
+   * */
 
   public setTitle(title: string): void {
     this.node.setTitle(title);
@@ -34,6 +53,13 @@ export class NodeBuilder implements PluginContextBuilder {
     this.node.setDescription(description);
   }
 
+  /**
+   *
+   * @param plugin Plugin name
+   * @param name Unique name for the node
+   * @throws Error if plugin or name is not instantiated
+   * */
+
   public instantiate(plugin: string, name: string): void {
     if (plugin === "" || name === "") {
       throw new Error("Plugin or name is not instantiated");
@@ -42,17 +68,41 @@ export class NodeBuilder implements PluginContextBuilder {
     this.node.instantiate(plugin, name);
   }
 
+  /**
+   *
+   * @param icon Icon to be displayed on the node
+   *
+   */
+
   public addIcon(icon: string): void {
     this.node.setIcon(icon);
   }
+
+  /**
+   * @param type type of input
+   * @param identifier  Unique identifier for the node
+   * @param displayName Node name to display
+   */
 
   public addInput(type: string, identifier: string, displayName: string): void {
     this.node.addInput(type, identifier, displayName);
   }
 
+  /**
+   *
+   * @param type type of output
+   * @param identifier  Unique identifier for the node
+   * @param displayName Node name to display
+   */
+
   public addOutput(type: string, identifier: string, displayName: string): void {
     this.node.addOutput(type, identifier, displayName);
   }
+
+  /**
+   * Creates a new nodeUIBuilder for the node
+   * @returns NodeUIBuilder
+   */
 
   public createUIBuilder(): NodeUIBuilder {
     const builder = new NodeUIBuilder();
@@ -61,14 +111,32 @@ export class NodeBuilder implements PluginContextBuilder {
     return builder;
   }
 
+  /**
+   * This function is used to define the functionality of the node
+   * @param code This is the code that will be executed when the node is called
+   *
+   * */
+
   public define(code: any) {
     this.node.setFunction(code);
   }
+
+  /**
+   *
+   * Sets the nodeUIBuilder for the node
+   * @param ui NodeUIBuilder that will be used to build the UI
+   * */
 
   public setUI(ui: NodeUIBuilder) {
     this.ui = ui.getUI();
   }
 }
+
+/**
+ *
+ * Builds the nodes's ui through restricted interface
+ *
+ * */
 
 export class NodeUIBuilder {
   private node: NodeUIParent;
@@ -77,10 +145,27 @@ export class NodeUIBuilder {
     this.node = new NodeUIParent("", null);
   }
 
+  /**
+   *
+   * @param label Label for the button
+   * @param param Parameter for the button
+   * @returns callback to this NodeUIBuilder
+   * */
+
   public addButton(label: string, param: any): NodeUIBuilder {
     this.node.params.push(new NodeUILeaf(NodeUIComponent.Button, label, [param], this.node));
     return this;
   }
+
+  /**
+   *
+   * @param label Label for the slider
+   * @param min Minimum value for the slider
+   * @param max Maximum value for the slider
+   * @param step Change in value for the slider
+   * @param defautlVal Default value
+   * @returns callback to this NodeUIBuilder
+   */
 
   public addSlider(
     label: string,
@@ -96,6 +181,14 @@ export class NodeUIBuilder {
     return this;
   }
 
+  /**
+   *
+   * @param label Label for the dropdown
+   * @param builder NodeUIBuilder for the dropdown
+   * @returns callback to this NodeUIBuilder
+   *
+   * */
+
   public addDropdown(label: string, builder: NodeUIBuilder): NodeUIBuilder {
     builder.node.label = label;
     builder.node.parent = this.node;
@@ -103,15 +196,36 @@ export class NodeUIBuilder {
     return this;
   }
 
+  /**
+   *
+   * @param label Label for the text input
+   * @returns callback to this NodeUIBuilder
+   *
+   * */
+
   public addNumberInput(label: string): NodeUIBuilder {
     this.node.params.push(new NodeUILeaf(NodeUIComponent.NumberInput, label, [], this.node));
     return this;
   }
 
+  /**
+   *
+   * @param label Label for the text input
+   * @returns callback to this NodeUIBuilder
+   *
+   * */
+
   public addImageInput(label: string): NodeUIBuilder {
     this.node.params.push(new NodeUILeaf(NodeUIComponent.FilePicker, label, [], this.node));
     return this;
   }
+
+  /**
+   *
+   * @param label Label for the text input
+   * @returns callback to this NodeUIBuilder
+   *
+   * */
 
   // We need to discuss how to handle color pickers
   public addColorPicker(label: string, param: any): NodeUIBuilder {
@@ -123,6 +237,14 @@ export class NodeUIBuilder {
   public getUI() {
     return this.node;
   }
+
+  /**
+   *
+   * @param label Label for the text input
+   * @param param Parameter for the text input
+   * @returns callback to this NodeUIBuilder
+   *
+   * */
 
   public addLabel(label: string, param: string) {
     this.node.params.push(new NodeUILeaf(NodeUIComponent.Label, label, [param], this.node));
