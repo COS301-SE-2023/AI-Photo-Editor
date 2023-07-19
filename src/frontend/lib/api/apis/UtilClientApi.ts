a16bea92f3b1dfc1a27201b8a0b4953ba29be20b
Armand Krynauw <armand.krynauw@gmail.com>
2023-07-19 10:35:23 +0200

    Attempt to fix Blix instantiation order

--- src/frontend/lib/api/apis/UtilClientApi.ts
+++ src/frontend/lib/api/apis/UtilClientApi.ts
@@ -1,8 +1,14 @@
 import type { ElectronWindowApi } from "electron-affinity/window";
 import { toastStore, type ToastOptions } from "../../stores/ToastStore";
+import { blixStore, setInitialStores } from "../../../lib/stores/BlixStore";
 
 export class UtilClientApi implements ElectronWindowApi<UtilClientApi> {
   showToast(options: Partial<ToastOptions>) {
     toastStore.trigger(options);
+  }
+
+  async onBlixReady() {
+    await setInitialStores();
+    blixStore.update((state) => ({ ...state, blixReady: true }));
   }
 }
