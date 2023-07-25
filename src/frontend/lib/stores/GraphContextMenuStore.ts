efd17f3735a0289f659f1898d7383014785516f1
Armand Krynauw <armand.krynauw@gmail.com>
2023-07-25 19:45:05 +0200

    Clean up Graph context menu and add actual plugin nodes

--- src/frontend/lib/stores/GraphContextMenuStore.ts
+++ src/frontend/lib/stores/GraphContextMenuStore.ts
@@ -1,15 +1,18 @@
-import { writable } from "svelte/store";
-export type Coordinates = {
-  x: number;
-  y: number;
-};
+import { writable, get } from "svelte/store";
+import { toolboxStore } from "./ToolboxStore";
+import { INode } from "@shared/ui/ToolboxTypes";
 
 export type ContextMenuState = {
   isShowing: boolean;
-  windowPos: Coordinates;
+  cursorPos: Coordinates;
   canvasPos: Coordinates;
   graphId: string;
   items: (ItemGroup | Item)[];
+};
+
+export type Coordinates = {
+  x: number;
+  y: number;
 };
 
 export type ItemGroup = {
@@ -23,24 +26,24 @@ export type Item = {
   action: Action;
 };
 
-export type CustomEvent = { action: Item };
 export type Action = { type: "addNode"; signature: string };
 
 class ContextMenuStore {
   // Pixel Dimensions of Screen as numbers
   private screenDimensions = { width: 0, height: 0 };
   // Pixel Dimensions of Menu as numbers
-  private menuDimensions = { width: 192, height: 259 };
+  private menuDimensions = { width: 192, height: 256 };
   // Pixel count Padding between Menu and Screen as number
   private screenPadding = 15;
 
   private readonly store = writable<ContextMenuState>({
     isShowing: false,
-    windowPos: { x: 0, y: 0 },
+    cursorPos: { x: 0, y: 0 },
     canvasPos: { x: 0, y: 0 },
     graphId: "",
     items: [],
   });
+
   /**
    * This function is used to set the screen dimensions
    *
@@ -48,13 +51,23 @@ class ContextMenuStore {
    * @param height New screen height
    */
   public setScreenDimensions(width: number, height: number) {
+    const deltaX = this.screenDimensions.width - width;
+    const deltaY = this.screenDimensions.height - height;
     this.screenDimensions.width = width;
     this.screenDimensions.height = height;
+
+    this.store.update((state) => {
+      const { x, y } = state.cursorPos;
+      state.cursorPos.x = x - deltaX;
+      state.cursorPos.y = y - deltaY;
+      return state;
+    });
   }
 
   public get subscribe() {
     return this.store.subscribe;
   }
+
   /**
    * This function sets the state of the menu before is is shown to the user
    * Before is is shown the current coordinates of the top left corner of the menu are checked
@@ -63,45 +76,49 @@ class ContextMenuStore {
    *
    * @param state State of menu to be shown
    */
-  public showMenu(state: ContextMenuState) {
+  public showMenu(cursorPos: Coordinates, canvasPos: Coordinates, graphId: string) {
+    const toolbox = get(toolboxStore);
+    const menuNodes = this.groupBy(Object.values(toolbox));
+
     // Max Length and height of current application screen
-    const { windowPos } = state;
     // 1 - Recalculate menu position for horizontal overflow - Right Border
     // 2 - Recalculate menu position for vertical overflow - Left border
     if (
-      windowPos.x + this.menuDimensions.width + this.screenPadding >
+      cursorPos.x + this.menuDimensions.width + this.screenPadding >
       this.screenDimensions.width
     ) {
-      state.windowPos.x =
-        this.screenDimensions.width - this.menuDimensions.width - this.screenPadding;
-    } else if (windowPos.x - this.screenPadding < 0) {
-      state.windowPos.x = this.screenPadding;
+      cursorPos.x = this.screenDimensions.width - this.menuDimensions.width - this.screenPadding;
+    } else if (cursorPos.x - this.screenPadding < 0) {
+      cursorPos.x = this.screenPadding;
     }
     // 1 - Recalculate menu position for vertical overflow - Bottom Border
     // 2 - Recalculate menu position for horizontal overflow - Top border
     if (
-      windowPos.y + this.menuDimensions.height + this.screenPadding >
+      cursorPos.y + this.menuDimensions.height + this.screenPadding >
       this.screenDimensions.height
     ) {
-      state.windowPos.y =
-        this.screenDimensions.height - this.menuDimensions.height - this.screenPadding;
-    } else if (windowPos.y - this.screenPadding < 0) {
-      state.windowPos.y = this.screenPadding;
+      cursorPos.y = this.screenDimensions.height - this.menuDimensions.height - this.screenPadding;
+    } else if (cursorPos.y - this.screenPadding < 0) {
+      cursorPos.y = this.screenPadding;
     }
-    this.store.set(state);
-  }
-  /**
-   * This function will rest the state of the menu to its default values
-   */
-  public hideMenu() {
+
     this.store.set({
-      isShowing: false,
-      windowPos: { x: 0, y: 0 },
-      canvasPos: { x: 0, y: 0 },
-      graphId: "",
-      items: [],
+      isShowing: true,
+      cursorPos,
+      canvasPos,
+      graphId,
+      items: menuNodes,
     });
   }
+
+  public hideMenu() {
+    this.store.update((state) => {
+      state.isShowing = false;
+      state.items = [];
+      return state;
+    });
+  }
+
   /**
    * This function will show and hide the menu
    */
@@ -111,6 +128,33 @@ class ContextMenuStore {
       return state;
     });
   }
+
+  private groupBy(nodes: INode[]): ItemGroup[] {
+    const groups: Record<string, ItemGroup> = {};
+
+    for (const node of nodes) {
+      const parts = node.signature.split(".");
+      const plugin = parts[0];
+
+      if (!groups[plugin]) {
+        groups[plugin] = {
+          label: plugin,
+          items: [],
+        };
+      }
+
+      groups[plugin].items.push({
+        label: node.title,
+        icon: node.icon,
+        action: {
+          type: "addNode",
+          signature: node.signature,
+        },
+      });
+    }
+
+    return Object.values(groups);
+  }
 }
 
-export const graphNodeMenuStore = new ContextMenuStore();
+export const graphMenuStore = new ContextMenuStore();
--- src/frontend/ui/utils/ContextMenuGroup.svelte
+++ src/frontend/ui/utils/ContextMenuGroup.svelte
@@ -25,28 +25,33 @@
       on:keydown="{null}"
     >
       {#if expanded}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="h-4 w-4 stroke-purple-500"
-        >
-          <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-          ></path>
-        </svg>
+        <div class="z-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="h-4 w-4 stroke-purple-500"
+          >
+            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+            ></path>
+          </svg>
+        </div>
       {:else}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="h-4 w-4 stroke-purple-500"
-        >
-          <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5"></path>
-        </svg>
+        <div class="z-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="h-4 w-4 stroke-purple-500"
+          >
+            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5"
+            ></path>
+          </svg>
+        </div>
       {/if}
       <div class="text-zinc-400">{root.label}</div>
     </div>
