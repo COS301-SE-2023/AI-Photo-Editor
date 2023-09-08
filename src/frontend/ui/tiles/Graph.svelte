efe161b11c5f2b05fe44d71aaab553fff0fcf473
Armand Krynauw <armand.krynauw@gmail.com>
2023-09-08 06:03:34 +0200

    Fix styling on undo/redo buttons

--- src/frontend/ui/tiles/Graph.svelte
+++ src/frontend/ui/tiles/Graph.svelte
@@ -266,7 +266,7 @@
       viewBox="0 0 24 24"
       stroke-width="1.5"
       stroke="currentColor"
-      class="h-6 w-6"
+      class="h-5 w-5 stroke-zinc-400"
     >
       <path
         stroke-linecap="round"
@@ -286,7 +286,7 @@
       viewBox="0 0 24 24"
       stroke-width="1.5"
       stroke="currentColor"
-      class="h-6 w-6"
+      class="h-5 w-5 stroke-zinc-400"
     >
       <path
         stroke-linecap="round"
