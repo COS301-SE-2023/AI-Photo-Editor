7222aa443724649bc02ae0ab155eed9ded8d7faa
Jake Mileham <jake.mileham@gmail.com>
2023-09-25 20:22:06 +0200

    Cleanup cache picker

--- src/frontend/ui/utils/graph/nodeUIComponents/CachePicker.svelte
+++ src/frontend/ui/utils/graph/nodeUIComponents/CachePicker.svelte
@@ -2,6 +2,7 @@
   import { writable } from "svelte/store";
   import { UIValueStore } from "@shared/ui/UIGraph";
   import { cacheStore } from "../../../../lib/stores/CacheStore";
+  import { blixStore } from "../../../../lib/stores/BlixStore";
   import type { UIComponentConfig, UIComponentProps } from "@shared/ui/NodeUITypes";
   import { createEventDispatcher } from "svelte";
 
@@ -29,16 +30,20 @@
   {#key inputStore.inputs[config.componentId]}
     <select bind:value="{$valStore}" on:change="{handleInputInteraction}">
       {#each Object.keys(items) as itemKey}
-        <option value="{itemKey}"
-          >{items[itemKey].name} [{itemKey}] ({items[itemKey].contentType})</option
-        >
+        {#if !$blixStore.production}
+          <option value="{itemKey}"
+            >{items[itemKey].name} [{itemKey}] ({items[itemKey].contentType})</option
+          >
+        {:else}
+          <option value="{itemKey}">{items[itemKey].name}</option>
+        {/if}
       {/each}
     </select>
   {/key}
 {:else}
   <!-- No items to display -->
   <select>
-    <option selected disabled>-</option>
+    <option selected disabled>Add an asset</option>
   </select>
 {/if}
 
