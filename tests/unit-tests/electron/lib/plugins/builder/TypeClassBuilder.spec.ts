fa659f3b00df759e895786ef5ecf61bcb0ea6551
CenturionLC <johnshepard357@gmail.com>
2023-09-26 11:19:10 +0200

    TypeClassBuilder finished

--- tests/unit-tests/electron/lib/plugins/builder/TypeClassBuilder.spec.ts
+++ tests/unit-tests/electron/lib/plugins/builder/TypeClassBuilder.spec.ts
@@ -1,5 +1,8 @@
 import { type } from "os";
 import {TypeclassBuilder} from "../../../../../../src/electron/lib/plugins/builders/TypeclassBuilder"
+import { MediaDisplayConfig, MediaDisplayType } from "../../../../../../src/shared/types";
+import { Typeclass } from "../../../../../../src/electron/lib/registries/TypeclassRegistry";
+import type { ConverterTriple } from "../../../../../../src/electron/lib/registries/TypeclassRegistry";
 
 
 describe("TypeClassBuilder", () => {
@@ -27,6 +30,31 @@ describe("TypeClassBuilder", () => {
         typeclassBuilder.setToConverters({"test2": fn1});
         expect(typeclassBuilder["partial"].toConverters).toEqual([["test2", fn1]]);
 
+
+        const configurator: (data: string) => MediaDisplayConfig = (data: string) => {
+            return {
+                displayType: MediaDisplayType.TextBox,
+                props: { content: "Invalid typeclass: Media display not specified" },
+                contentProp: null,
+                } as MediaDisplayConfig;
+        }
+
+        typeclassBuilder.setDisplayConfigurator(configurator);
+        expect(typeclassBuilder["partial"].mediaDisplayConfigurator("a")).toEqual(configurator("a"));
+
+        const build : Typeclass = {
+            id: "number",
+            description: "test description",
+            subtypes: [],
+            mediaDisplayConfig: configurator,
+        }
+
+        expect(typeclassBuilder["buildTypeclass"]).toEqual(build);
+
+        const converters : ConverterTriple[] = [["test1",typeclassBuilder["partial"].id, fn], [typeclassBuilder["partial"].id,"test2", fn1]];
+        expect(typeclassBuilder["buildConverters"]).toEqual(converters);
+
+        expect(typeclassBuilder["build"]).toEqual([build,converters]);  
     })
 
 
