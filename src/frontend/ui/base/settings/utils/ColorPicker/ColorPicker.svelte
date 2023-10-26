ba1b7996604f79db30c3a711dbebf135718b1ef0
Armand Krynauw <armand.krynauw@gmail.com>
2023-10-27 00:11:52 +0200

    Hot fix for accent colors

--- src/frontend/ui/base/settings/utils/ColorPicker/ColorPicker.svelte
+++ src/frontend/ui/base/settings/utils/ColorPicker/ColorPicker.svelte
@@ -1,29 +1,8 @@
 <script lang="ts">
-  import { tick } from "svelte";
-  import ColorPickerComponent from "svelte-awesome-color-picker";
   import { type ColorPicker } from "../../../../../../shared/types";
-  import ColorPickerWrapper from "./ColorPickerWrapper.svelte";
-  import ColorPickerTextInput from "./ColorPickerTextInput.svelte";
 
   export let item: ColorPicker;
   let inputElement: HTMLInputElement;
-  let test: string;
-
-  async function handleChange(event: Event) {
-    console.log("here");
-    if (item.onChange) {
-      // await tick();
-      item.onChange(item.value);
-    }
-  }
-
-  $: {
-    if (item.onChange) {
-      item.onChange(item.value);
-    }
-  }
-
-  $: console.log(test);
 
   function handleClick() {
     inputElement.style.position = "absolute";
@@ -31,27 +10,12 @@
   }
 </script>
 
-<!-- <div id="settings-portal"></div>
-
-<ColorPickerComponent
-  bind:hex="{item.value}"
-  isAlpha="{false}"
-  label=""
-  components="{{ wrapper: ColorPickerWrapper, textInput: ColorPickerTextInput }}"
-/> -->
-
 <div
-  class="bg-primary-500 h-8 w-8 rounded-full border-2 border-zinc-600"
+  class="h-8 w-8 rounded-full border-2 border-zinc-600 bg-primary-500"
   on:click="{handleClick}"
   on:keydown="{null}"
 >
-  <input
-    class="invisible"
-    type="color"
-    bind:this="{inputElement}"
-    on:change="{handleChange}"
-    bind:value="{item.value}"
-  />
+  <input class="invisible" type="color" bind:this="{inputElement}" bind:value="{item.value}" />
 </div>
 
 <style lang="scss">
